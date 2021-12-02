be255b30884270485e502af94e79250a01eaba4d
Tao Wen <taowen@chengfayun.com>
2021-12-02 16:28:11 +0800

    add iter_tests

---
+++ iter_tests/string_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	jsoniter "github.com/json-iterator/tinygo"
+)
+
+func Test_valid_string(t *testing.T) {
+	iter := jsoniter.ParseBytes([]byte(`"hello"`))
+	if iter.WhatIsNext() != jsoniter.StringValue {
+		t.Fail()
+	}
+	val := iter.ReadString()
+	if val != "hello" {
+		t.Fail()
+	}
+}
+
+func Test_string_missing_starting_quote(t *testing.T) {
+	iter := jsoniter.ParseBytes([]byte(`1,2`))
+	iter.ReadString()
+	if iter.Error == nil {
+		t.Fail()
+	}
+}
+
+func Test_null_as_string(t *testing.T) {
+	iter := jsoniter.ParseBytes([]byte(`null`))
+	if iter.WhatIsNext() != jsoniter.NullValue {
+		t.Fail()
+	}
+	if iter.ReadString() != "" {
+		t.Fail()
+	}
+	if iter.Error != nil {
+		t.Fail()
+	}
+}
